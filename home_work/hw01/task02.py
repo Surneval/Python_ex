b1893d149f3e327001e8101085e10ce3fb3adef9
Surneval <nadia.trusova@yahoo.com>
2022-12-12 16:46:04 +0300

    Task 2 is done

---
+++ home_work/hw01/task02.py
@@ -0,0 +1,23 @@
+#Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z
+# для всех значений предикат
+
+def input_numbers(q):
+    xyz = ['x', 'y', 'z']
+    values = []
+    for i in range(q):
+        values.append(input(f"Enter {xyz[i]}: "))
+    return values
+
+def check_predicate(values):
+    left = not(values[0] or values[1] or values [2])
+    right = not values[0] and not values[1] and not values[2]
+    result = left == right
+    return result
+
+user_statem = input_numbers(q=3)
+if check_predicate(user_statem) == True:
+    print("The statement is True")
+else:
+    print("The statement is false")
+
+
